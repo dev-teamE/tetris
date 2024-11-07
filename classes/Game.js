5d528cd77f5ba91f8526ac5fad00adf6163f0ff5
Shinto T. <165347789+shntt@users.noreply.github.com>
2024-11-07 22:05:20 +0900

    Update Game.js

--- classes/Game.js
+++ classes/Game.js
@@ -4,7 +4,7 @@ export class Game {
   currentTime;
   animationId;
 
-  constructor(bgm_sound, drop_sound, hold_sound, clear_sound, move_sound, rotate_sound) {
+  constructor() {
     this.lastTime = 0;
     this.gameActive = true;
     this.pauseStartTime = null;
